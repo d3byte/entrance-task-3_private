a6632dcf1e75701c8b394d755b93a00491b78f4c
Pandinator <cheloveckov.ru@yandex.ru>
2018-01-15 23:08:53 +0300

    Иницилизировал Apollo

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' })
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root')
+)
 registerServiceWorker()
