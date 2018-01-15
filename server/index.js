cec4e7ec46f2cc603635c897316d30b62102b360
Pandinator <cheloveckov.ru@yandex.ru>
2018-01-15 23:10:04 +0300

    поменял порт у сервера

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ const bodyParser = require('body-parser');
 const pagesRoutes = require('./pages/routes');
 const graphqlRoutes = require('./graphql/routes');
 
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -16,4 +20,4 @@ app.use('/', pagesRoutes);
 app.use('/graphql', graphqlRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => console.log('Express app listening on localhost:3000'));
+app.listen(4000, () => console.log('Express app listening on localhost:4000'));
